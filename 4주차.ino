c3c3271fca3cce6d249ae29000ce4a72e4976c4b
alwayshyeon <alwayshyeon19@gmail.com>
2025-03-14 13:05:37 +0900

    Add files via upload

---
+++ 4주차.ino
@@ -0,0 +1,23 @@
+int red = 12;
+int yellow = 11;
+int green = 10;
+void setup(){
+  pinMode(red,OUTPUT);
+  pinMode(yellow,OUTPUT);
+  pinMode(green,OUTPUT);
+}
+
+void loop(){
+  digitalWrite(red,HIGH);
+  digitalWrite(yellow,LOW);
+  digitalWrite(green,LOW);
+  delay(1000);   
+  digitalWrite(red,LOW);
+  digitalWrite(yellow,HIGH);
+  digitalWrite(green,LOW);
+  delay(1000);
+  digitalWrite(red,LOW);
+  digitalWrite(yellow,LOW);
+  digitalWrite(green,HIGH);
+  delay(1000);
+}
